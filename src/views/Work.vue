efeded6ed3c593f8fa7c46b03e0e73722feb8314
Genshi0916 <genshi0916@gmail.com>
2020-07-23 15:30:27 +0900

    修正

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -4,15 +4,15 @@
     <v-layout justify-center>
       <v-card class="pa-6" id="card">
         <v-layout row wrap>
-          <v-flex  v-for="(item,i) in list" :key="i">
+          <v-flex  v-for="(item, i) in items" :key="i">
             <v-card class="grey lighten-3 ma-4">
               <v-container>
                 <v-card-text  id="center">
                   <v-col>
                   <p align="center" id="title">{{item.title}}</p>
                   <v-layout justify-center>
-                    <a @click="jumpLink(link[i])">
-                      <img :src="image[i]" id="cardImage">
+                    <a v-bind:href="item.url" target="_blank">
+                      <v-img :src="item.icon" id="cardImage"></v-img>
                     </a>
                   </v-layout>
                   </v-col>
@@ -31,47 +31,56 @@
 <script>
   export default{
     data:()=>({
-        list:[
-          {title:'FuraIT',comment:'僕が富良野市出身という縁もあって勉強会などでお手伝いさせてもらっています'},
-          {title:'旭川ゆるい勉強会',comment:'旭川高専に通っているということもありまわりの友人たちも誘って勉強会のお手伝いをさせてもらっています'},
-          {title:'第3回 全国高専ビジネスコンテストby高専キャリア',comment:'チーム「よっぴー」MVT,サンケイエンジニアリング賞を受賞しました'},
-          {title:'第30回高専プロコン',comment:'競技部門に参加する予定だったが台風19号により移動部門敗退'},
-          {title:'高専カンファin北海道',comment:'運営陣として参加し司会をしました'},
-        ],
-        image:[
-          require("../assets/furait.jpg"),
-          require("../assets/yurui.png"),
-          require("../assets/bcon3.jpg"),
-          require("../assets/procon30.jpg"),
-          require("../assets/kosen_konf_hokkaido.png"),
-        ],
-        link:[
-          "https://furait.connpass.com/",
-          "https://asahikawa.connpass.com/",
-          "https://third-kosen-bcon.peatix.com/view",
-          "http://www.procon.gr.jp/?page_id=71201",
-          "https://conf-hokkaido.connpass.com/event/122809/",
-        ],
+      items:
+      [
+        {
+          title:"FuraIT",
+          comment:'僕が富良野市出身という縁もあって勉強会などでお手伝いさせてもらっています',
+          url:"https://furait.connpass.com/",
+          icon:require("../assets/furait.jpg")
+        },
+        {
+          title:"旭川ゆるい勉強会",
+          comment:'旭川高専に通っているということもありまわりの友人たちも誘って勉強会のお手伝いをさせてもらっています',
+          url:"https://asahikawa.connpass.com/",
+          icon:require("../assets/yurui.png")
+        },
+        {
+          title:"第3回 全国高専ビジネスコンテストby高専キャリア",
+          comment:'チーム「よっぴー」MVT,サンケイエンジニアリング賞を受賞しました',
+          url:"https://third-kosen-bcon.peatix.com/view",
+          icon:require("../assets/bcon3.jpg")
+        },
+                {
+          title:"第30回高専プロコン",
+          comment:'競技部門に参加する予定だったが台風19号により移動部門敗退',
+          url:"http://www.procon.gr.jp/?page_id=71201",
+          icon:require("../assets/procon30.jpg")
+        },
+        {
+          title:"高専カンファin北海道",
+          comment:'運営陣として参加し司会をしました',
+          url:"https://conf-hokkaido.connpass.com/event/122809/",
+          icon:require("../assets/kosen_konf_hokkaido.png")
+        }
+      ]
       }),
-    methods:{
-      jumpLink(link){
-        window.open(link)
-      }
-    },
 
-      props: {
-    flat: Boolean,
-    hover: Boolean,
-    img: String,
-    link: Boolean,
-    loaderHeight: {
-      type: [Number, String],
-      default: 4,
+    props:
+    {
+      flat: Boolean,
+      hover: Boolean,
+      img: String,
+      link: Boolean,
+      loaderHeight:
+      {
+        type: [Number, String],
+        default: 4,
+      },
+      outlined: Boolean,
+      raised: Boolean,
+      shaped: Boolean,
     },
-    outlined: Boolean,
-    raised: Boolean,
-    shaped: Boolean,
-  },
   }
 </script>
 <style scoped>
