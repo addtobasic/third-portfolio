1e1a06a6d60b506f470a582dc25abc09e2a2c6a1
Genshi0916 <genshi0916@gmail.com>
2021-01-08 15:21:45 +0900

    :sparkles: Contactの完成

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,14 +1,101 @@
 <template>
-  <v-card>
-    <p>contact</p>
-  </v-card>
+  <div>
+    <v-card dark>
+      <div id="contactMain">
+        <h1 align="center" id="title">Contact</h1>
+        <v-container style="max-width: 600px;">
+          <fade-in-component>
+            <section>
+              <v-card>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12">
+                        <v-text-field label="お名前" required v-model="name"></v-text-field>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-text-field label="メールアドレス" required v-model="email"></v-text-field>
+                      </v-col>
+                      <v-col cols="12">
+                        <v-textarea label="問い合わせ内容" outlined required v-model="description"></v-textarea>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn  v-on:click="submit" color="">Send</v-btn>
+                </v-card-actions>
+              </v-card>
+            </section>
+          </fade-in-component>
+        </v-container>
+      </div>
+      <div id="copyRight">
+        <h5 align="center">icon by @V_Melville</h5>
+        <h4 align="center">© 2020 Genshi0916</h4>
+      </div>
+    </v-card>
+  </div>
 </template>
-
 <script>
+  import FadeInComponent from '../components/FadeInComponent'
+  import url from './webhookUrl'
   export default{
-    name:'Contact',
+    components:{
+      FadeInComponent,
+    },
+
+    data:()=>({
+      dialog:true,
+      name:'',
+      email:'',
+      description:''
+    }),
+
+    methods:{
+      submit:function(){
+        var name = this.name
+        var email = this.email
+        var description = this.description
+        const payload = {
+          text: '問い合わせがありました\n'+
+                '名前:'+name+'\n'+
+                'email:'+email+'\n'+
+                '内容:'+description+'\n'
+        }
+        const url = url.WEBHOOKURL
+        console.log(url)
+        fetch(url,{
+          method:'POST',
+          body:JSON.stringify(payload)
+        }).then(()=>{
+          alert('送信完了');
+          this.name="";
+          this.email="";
+          this.description="";
+        })
+      }
+    }
   }
 </script>
-<style>
+<style scoped>
+  #contactMain{
+    padding-bottom: 30px;
+  }
+
+  #title{
+    padding:20px 0px;
+  }
+
+  #copyRight{
+    padding:20px 0px;
+
+  }
+
+/* aタグのデコレーションを消す */
+  a{
+    text-decoration: none;
+  }
 
 </style>
