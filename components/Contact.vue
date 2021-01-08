404e5e58ed401f893ccb2f9c459b32927c077f41
Genshi0916 <genshi0916@gmail.com>
2021-01-09 00:49:43 +0900

    :bug: webhookがうまく動かなかったので修正

--- components/Contact.vue
+++ components/Contact.vue
@@ -64,7 +64,6 @@
                 '内容:'+description+'\n'
         }
         const url = 'ここにはwebhookのurlが入る'
-        // const url = 'ここにはwebhookのurlが入る'
         fetch(url,{
           method:'POST',
           body:JSON.stringify(payload)
