93c0ab1a36031cc7e4d68b7e5fab02443407fab7
Genshi0916 <genshi0916@gmail.com>
2021-08-27 22:21:23 +0900

    :sparkles: Workの内容の追加

--- components/Work.vue
+++ components/Work.vue
@@ -1,273 +1,238 @@
 <template>
   <v-card light>
     <div id="workMain">
-      <h1 align="center" id="title">Work</h1>
+      <h1 id="title" align="center">Work</h1>
       <v-container style="max-width: 800px">
         <fade-in-component>
           <section>
             <v-card>
               <v-timeline dense clipped>
-                <v-timeline-item
-                  class="mb-6"
-                  hide-dot
-                >
+                <v-timeline-item class="mb-6" hide-dot>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2002 9/16
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      生誕
+                    <v-col cols="12" sm="3" md="3"> 2002 9/16 </v-col>
+                    <v-col cols="12" sm="9" md="9"> 生誕 </v-col>
+                  </v-row>
+                </v-timeline-item>
+                <v-timeline-item class="mb-4" color="#5BE7C4" small>
+                  <v-row justify="space-between">
+                    <v-col cols="12" sm="3" md="3"> 2018 3/4 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="https://furait.connpass.com/"
+                        target="_blank"
+                        rel="noreferrer"
+                        >FuraIT</a
+                      >
+                      参加
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#5BE7C4"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#50C1E9" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2018 3/4
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="https://furait.connpass.com/" target="_blank" rel="noreferrer">FuraIT</a> 参加
+                    <v-col cols="12" sm="3" md="3"> 2018 4/3 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="https://www.asahikawa-nct.ac.jp/"
+                        target="_blank"
+                        rel="noreferrer"
+                        >旭川工業高等専門学校</a
+                      >
+                      入学
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#50C1E9"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#7A57D1" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2018 4/3
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="https://www.asahikawa-nct.ac.jp/" target="_blank" rel="noreferrer">旭川工業高等専門学校</a> 入学
+                    <v-col cols="12" sm="3" md="3"> 2018 6/23 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="https://asahikawa.connpass.com/"
+                        target="_blank"
+                        rel="noreferrer"
+                        >旭川ゆるい勉強会</a
+                      >
+                      参加
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#7A57D1"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#5BE7C4" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2018 6/23
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="https://asahikawa.connpass.com/" target="_blank" rel="noreferrer">旭川ゆるい勉強会</a> 参加
+                    <v-col cols="12" sm="3" md="3"> 2018 7/23 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="https://students.local.or.jp/"
+                        target="_blank"
+                        rel="noreferrer"
+                        >LOCAL学生部</a
+                      >
+                      入部
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#5BE7C4"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#50C1E9" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2018 7/23
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="https://students.local.or.jp/" target="_blank" rel="noreferrer">LOCAL学生部</a> 入部
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#50C1E9"
-                  small
-                >
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2019 1/9
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
+                    <v-col cols="12" sm="3" md="3"> 2019 1/9 </v-col>
+                    <v-col cols="12" sm="9" md="9">
                       Kosen LT ONLINE 登壇
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#7A57D1"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#7A57D1" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2019 6/1
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="https://www.ospn.jp/osc2019-do/" target="_blank" rel="noreferrer">OSC19do</a> 当日スタッフ
+                    <v-col cols="12" sm="3" md="3"> 2019 6/1 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="https://www.ospn.jp/osc2019-do/"
+                        target="_blank"
+                        rel="noreferrer"
+                        >OSC19do</a
+                      >
+                      当日スタッフ
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#5BE7C4"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#5BE7C4" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2019 8/17
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="https://conf-hokkaido.connpass.com/event/122809/" target="_blank" rel="noreferrer">高専カンファ in北海道</a> 司会
+                    <v-col cols="12" sm="3" md="3"> 2019 8/17 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="https://conf-hokkaido.connpass.com/event/122809/"
+                        target="_blank"
+                        rel="noreferrer"
+                        >高専カンファ in北海道</a
+                      >
+                      司会
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#50C1E9"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#50C1E9" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2019 8/24
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="https://www.seccon.jp/2019/seccon_beginners/tomakomai.html" target="_blank" rel="noreferrer">SECCON Beginners 2019 苫小牧</a> 参加
+                    <v-col cols="12" sm="3" md="3"> 2019 8/24 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="https://www.seccon.jp/2019/seccon_beginners/tomakomai.html"
+                        target="_blank"
+                        rel="noreferrer"
+                        >SECCON Beginners 2019 苫小牧</a
+                      >
+                      参加
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#7A57D1"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#7A57D1" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2019 10/13
-                    </v-col>
-                    <v-col
-                      ccols="12" sm="9" md="9"
-                    >
-                      <a href="http://www.procon.gr.jp/?page_id=71201" target="_blank" rel="noreferrer">第30回高専プロコン</a> 競技部門 台風19号により辞退 参加部門敗退
+                    <v-col cols="12" sm="3" md="3"> 2019 10/13 </v-col>
+                    <v-col ccols="12" sm="9" md="9">
+                      <a
+                        href="http://www.procon.gr.jp/?page_id=71201"
+                        target="_blank"
+                        rel="noreferrer"
+                        >第30回高専プロコン</a
+                      >
+                      競技部門 台風19号により辞退 参加部門敗退
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#5BE7C4"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#5BE7C4" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2019 11/3
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="http://www.procon-asahikawa.org/" target="_blank" rel="noopener noreferrer">第9回 旭川 U-16 procon</a> スタッフ
+                    <v-col cols="12" sm="3" md="3"> 2019 11/3 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="http://www.procon-asahikawa.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >第9回 旭川 U-16 procon</a
+                      >
+                      スタッフ
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#50C1E9"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#50C1E9" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2020 3/11
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="https://third-kosen-bcon.peatix.com/view" target="_blank" rel="noopener noreferrer">第3回 高専ビジネスコンテスト by高専キャリア</a><br>チーム「よっぴー」MVT, サンケイエンジニアリング賞 受賞
+                    <v-col cols="12" sm="3" md="3"> 2020 3/11 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="https://third-kosen-bcon.peatix.com/view"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >第3回 高専ビジネスコンテスト by高専キャリア</a
+                      ><br />チーム「よっぴー」MVT, サンケイエンジニアリング賞
+                      受賞
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#7A57D1"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#7A57D1" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2020 6/27
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="https://ospn.connpass.com/event/177262/" target="_blank" rel="noopener noreferrer">OSC20do</a> LOCAL学生部 発表
+                    <v-col cols="12" sm="3" md="3"> 2020 6/27 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="https://ospn.connpass.com/event/177262/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >OSC20do</a
+                      >
+                      LOCAL学生部 発表
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#5BE7C4"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#5BE7C4" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2020 11/3
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="http://www.procon-asahikawa.org/" target="_blank" rel="noopener noreferrer">第10回 旭川 U-16 procon</a> スタッフ 実況
+                    <v-col cols="12" sm="3" md="3"> 2020 11/3 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="http://www.procon-asahikawa.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >第10回 旭川 U-16 procon</a
+                      >
+                      スタッフ 実況
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#50C1E9"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#50C1E9" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2021 2/26
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="https://jig.jp/topics/202012252000/" target="_blank" rel="noopener noreferrer">2021年冬jig.jpインターン</a> 参加
+                    <v-col cols="12" sm="3" md="3"> 2021 2/26 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="https://jig.jp/topics/202012252000/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >2021年冬jig.jpインターン</a
+                      >
+                      参加
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  class="mb-4"
-                  color="#7A57D1"
-                  small
-                >
+                <v-timeline-item class="mb-4" color="#7A57D1" small>
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3">
-                      2021 6/26
-                    </v-col>
-                    <v-col
-                      cols="12" sm="9" md="9"
-                    >
-                      <a href="https://www.tmcseec.net/ja/cst4u25_2021" target="_blank" rel="noopener noreferrer">サイバーセキュリティTOKYO for U25</a> 参加
+                    <v-col cols="12" sm="3" md="3"> 2021 6/26 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="https://www.tmcseec.net/ja/cst4u25_2021"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >サイバーセキュリティTOKYO for U25</a
+                      >
+                      参加
                     </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item
-                  hide-dot
-                  small
-                >
-                  and now...
+                <v-timeline-item class="mb-4" color="#5BE7C4" small>
+                  <v-row justify="space-between">
+                    <v-col cols="12" sm="3" md="3"> 2021 8/16~ 27 </v-col>
+                    <v-col cols="12" sm="9" md="9">
+                      <a
+                        href="https://www.lateral-t.co.jp/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        ラテラル・シンキング株式会社 インターン</a
+                      >
+                      参加
+                    </v-col>
+                  </v-row>
                 </v-timeline-item>
+                <v-timeline-item hide-dot small> and now... </v-timeline-item>
               </v-timeline>
             </v-card>
           </section>
@@ -277,25 +242,25 @@
   </v-card>
 </template>
 <script>
-  import FadeInComponent from '../components/FadeInComponent'
-  export default{
-    components:{
-      FadeInComponent,
-    },
-  }
+import FadeInComponent from '../components/FadeInComponent'
+export default {
+  components: {
+    FadeInComponent,
+  },
+}
 </script>
 <style scoped>
-  #workMain{
-    padding-bottom: 50px;
-  }
+#workMain {
+  padding-bottom: 50px;
+}
 
-  #title{
-    color: #35495e;
-    padding:20px 0px;
-  }
+#title {
+  color: #35495e;
+  padding: 20px 0px;
+}
 
 /* aタグのデコレーションを消す */
-  a{
-    text-decoration: none;
-  }
+a {
+  text-decoration: none;
+}
 </style>
