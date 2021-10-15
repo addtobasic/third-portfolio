52e1660c5a16a0c8f703118bee325c64a2c377ac
Genshi0916 <genshi0916@gmail.com>
2021-10-15 23:27:28 +0900

    :sparkles: Workの内容の追加

--- components/Work.vue
+++ components/Work.vue
@@ -13,236 +13,20 @@
                     <v-col cols="12" sm="9" md="9"> 生誕 </v-col>
                   </v-row>
                 </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#5BE7C4" small>
+                <v-timeline-item
+                  v-for="(item, i) in items"
+                  :key="i"
+                  class="mb-4"
+                  :color="item.color"
+                  small
+                >
                   <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2018 3/4 </v-col>
+                    <v-col cols="12" sm="3" md="3">{{ item.date }}</v-col>
                     <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://furait.connpass.com/"
-                        target="_blank"
-                        rel="noreferrer"
-                        >FuraIT</a
-                      >
-                      参加
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#50C1E9" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2018 4/3 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://www.asahikawa-nct.ac.jp/"
-                        target="_blank"
-                        rel="noreferrer"
-                        >旭川工業高等専門学校</a
-                      >
-                      入学
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#7A57D1" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2018 6/23 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://asahikawa.connpass.com/"
-                        target="_blank"
-                        rel="noreferrer"
-                        >旭川ゆるい勉強会</a
-                      >
-                      参加
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#5BE7C4" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2018 7/23 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://students.local.or.jp/"
-                        target="_blank"
-                        rel="noreferrer"
-                        >LOCAL学生部</a
-                      >
-                      入部
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#50C1E9" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2019 1/9 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      Kosen LT ONLINE 登壇
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#7A57D1" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2019 6/1 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://www.ospn.jp/osc2019-do/"
-                        target="_blank"
-                        rel="noreferrer"
-                        >OSC19do</a
-                      >
-                      当日スタッフ
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#5BE7C4" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2019 8/17 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://conf-hokkaido.connpass.com/event/122809/"
-                        target="_blank"
-                        rel="noreferrer"
-                        >高専カンファ in北海道</a
-                      >
-                      司会
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#50C1E9" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2019 8/24 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://www.seccon.jp/2019/seccon_beginners/tomakomai.html"
-                        target="_blank"
-                        rel="noreferrer"
-                        >SECCON Beginners 2019 苫小牧</a
-                      >
-                      参加
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#7A57D1" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2019 10/13 </v-col>
-                    <v-col ccols="12" sm="9" md="9">
-                      <a
-                        href="http://www.procon.gr.jp/?page_id=71201"
-                        target="_blank"
-                        rel="noreferrer"
-                        >第30回高専プロコン</a
-                      >
-                      競技部門 台風19号により辞退 参加部門敗退
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#5BE7C4" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2019 11/3 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="http://www.procon-asahikawa.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        >第9回 旭川 U-16 procon</a
-                      >
-                      スタッフ
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#50C1E9" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2020 3/11 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://third-kosen-bcon.peatix.com/view"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        >第3回 高専ビジネスコンテスト by高専キャリア</a
-                      ><br />チーム「よっぴー」MVT, サンケイエンジニアリング賞
-                      受賞
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#7A57D1" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2020 6/27 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://ospn.connpass.com/event/177262/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        >OSC20do</a
-                      >
-                      LOCAL学生部 発表
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#5BE7C4" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2020 11/3 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="http://www.procon-asahikawa.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        >第10回 旭川 U-16 procon</a
-                      >
-                      スタッフ 実況
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#50C1E9" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2021 2/26 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://jig.jp/topics/202012252000/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        >2021年冬jig.jpインターン</a
-                      >
-                      参加
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#7A57D1" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2021 6/26 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://www.tmcseec.net/ja/cst4u25_2021"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        >サイバーセキュリティTOKYO for U25</a
-                      >
-                      参加
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#5BE7C4" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2021 8/16~ 27 </v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://www.lateral-t.co.jp/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        ラテラル・シンキング株式会社 インターン</a
-                      >
-                      参加
-                    </v-col>
-                  </v-row>
-                </v-timeline-item>
-                <v-timeline-item class="mb-4" color="#50C1E9" small>
-                  <v-row justify="space-between">
-                    <v-col cols="12" sm="3" md="3"> 2021 9/6~ 10</v-col>
-                    <v-col cols="12" sm="9" md="9">
-                      <a
-                        href="https://e-seikatsu.snar.jp/jobboard/detail.aspx?id=9BjwS-eTmr6U1o1M_6Imqg"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        >株式会社いい生活 インターン</a
-                      >
-                      参加
+                      <a :href="item.url" target="_blank" rel="noreferrer">{{
+                        item.aTagContent
+                      }}</a>
+                      {{ item.subContent }}
                     </v-col>
                   </v-row>
                 </v-timeline-item>
@@ -257,10 +41,143 @@
 </template>
 <script>
 import FadeInComponent from '../components/FadeInComponent'
+
 export default {
   components: {
     FadeInComponent,
   },
+
+  data: () => ({
+    items: [
+      {
+        color: '#5BE7C4',
+        date: '2018 3/4',
+        url: 'https://furait.connpass.com/',
+        aTagContent: 'FuraIT',
+        subContent: '参加',
+      },
+      {
+        color: '#50C1E9',
+        date: '2018 4/3',
+        url: 'https://www.asahikawa-nct.ac.jp/',
+        aTagContent: '旭川工業高等専門学校',
+        subContent: '入学',
+      },
+      {
+        color: '#7A57D1',
+        date: '2018 6/23',
+        url: 'https://asahikawa.connpass.com/',
+        aTagContent: '旭川ゆるい勉強会',
+        subContent: '参加',
+      },
+      {
+        color: '#5BE7C4',
+        date: '2018 7/23',
+        url: 'https://students.local.or.jp/',
+        aTagContent: 'LOCAL学生部',
+        subContent: '入部',
+      },
+      {
+        color: '#50C1E9',
+        date: '2019 1/9',
+        url: 'https://twitter.com/hashtag/KosenLTONLINE?src=hashtag_click',
+        aTagContent: 'Kosen LT ONLINE',
+        subContent: '登壇',
+      },
+      {
+        color: '#7A57D1',
+        date: '2019 6/1',
+        url: 'https://www.ospn.jp/osc2019-do/',
+        aTagContent: 'OSC19do',
+        subContent: '当日スタッフ',
+      },
+      {
+        color: '#5BE7C4',
+        date: '2019 8/17',
+        url: 'https://conf-hokkaido.connpass.com/event/122809/',
+        aTagContent: '高専カンファ in北海道',
+        subContent: '司会',
+      },
+      {
+        color: '#50C1E9',
+        date: '2019 8/24',
+        url: 'https://www.seccon.jp/2019/seccon_beginners/tomakomai.html',
+        aTagContent: 'SECCON Beginners 2019 苫小牧',
+        subContent: '参加',
+      },
+      {
+        color: '#7A57D1',
+        date: '2019 10/13',
+        url: 'http://www.procon.gr.jp/?page_id=71201',
+        aTagContent: '第30回高専プロコン',
+        subContent: '競技部門 台風19号により辞退 参加部門敗退',
+      },
+      {
+        color: '#5BE7C4',
+        date: '2019 11/3',
+        url: 'http://www.procon-asahikawa.org/',
+        aTagContent: '第9回 旭川 U-16 procon',
+        subContent: 'スタッフ',
+      },
+      {
+        color: '#50C1E9',
+        date: '2020 3/11',
+        url: 'https://third-kosen-bcon.peatix.com/view',
+        aTagContent: '第3回 高専ビジネスコンテスト by高専キャリア',
+        subContent: 'チーム「よっぴー」MVT, サンケイエンジニアリング賞受賞',
+      },
+      {
+        color: '#7A57D1',
+        date: '2020 6/27',
+        url: 'https://ospn.connpass.com/event/177262/',
+        aTagContent: 'OSC20do',
+        subContent: 'LOCAL学生部 発表',
+      },
+      {
+        color: '#5BE7C4',
+        date: '2020 11/3',
+        url: 'http://www.procon-asahikawa.org/',
+        aTagContent: '第10回 旭川 U-16 procon',
+        subContent: 'スタッフ 実況',
+      },
+      {
+        color: '#50C1E9',
+        date: '2021 2/26',
+        url: 'https://jig.jp/topics/202012252000/',
+        aTagContent: '2021年冬jig.jpインターン',
+        subContent: '参加',
+      },
+      {
+        color: '#7A57D1',
+        date: '2021 6/26',
+        url: 'https://www.tmcseec.net/ja/cst4u25_2021',
+        aTagContent: 'サイバーセキュリティTOKYO for U25',
+        subContent: '参加',
+      },
+      {
+        color: '#5BE7C4',
+        date: '2021 8/16~ 27',
+        url: 'https://www.lateral-t.co.jp/',
+        aTagContent: 'ラテラル・シンキング株式会社 インターン',
+        subContent: '参加',
+      },
+      {
+        color: '#50C1E9',
+        date: '2021 9/6~ 10',
+        url:
+          'https://e-seikatsu.snar.jp/jobboard/detail.aspx?id=9BjwS-eTmr6U1o1M_6Imqg',
+        aTagContent: '株式会社いい生活 インターン',
+        subContent: '参加',
+      },
+      {
+        color: '#7A57D1',
+        date: '2021 10/9',
+        url: 'https://www.procon.gr.jp/',
+        aTagContent: '第32回高専プロコン',
+        subContent: '競技部門',
+      },
+    ],
+  }),
 }
 </script>
 <style scoped>
