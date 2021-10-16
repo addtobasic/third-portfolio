f16c2b29576d1afde02b7d2595ec62459d7e9753
Genshi0916 <genshi0916@gmail.com>
2021-10-16 19:54:33 +0900

    :sparkles: 404の遷移先の修正

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-card outlined id="main" light>
+    <v-card id="main" outlined light>
       <div>
         <v-card-title class="headline">
           <div v-if="error.statusCode === 404">
@@ -11,20 +11,20 @@
           </div>
         </v-card-title>
         <v-card-subtitle>
-          <a href="https://genshi0916.github.io/">Go back to the top page.</a>
+          <a href="https://addtobasic.dev/">Go back to the top page.</a>
         </v-card-subtitle>
       </div>
-      <div class="animation" id="icon">
-        <img class="img1" src="../static/img/genshi_line_animated.svg" alt="genshi_svg">
-        <img class="img2" src="../static/img/icon.jpg" alt="icon_img">
+      <div id="icon" class="animation">
+        <img
+          class="img1"
+          src="../static/img/genshi_line_animated.svg"
+          alt="genshi_svg"
+        />
+        <img class="img2" src="../static/img/icon.jpg" alt="icon_img" />
       </div>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn
-            outlined
-            icon
-            @click="reload"
-        >
+        <v-btn outlined icon @click="reload">
           <v-icon>mdi-reload</v-icon>
         </v-btn>
       </v-card-actions>
@@ -32,170 +32,175 @@
   </v-app>
 </template>
 <script>
-
 export default {
   layout: 'empty',
   props: {
     error: {
       type: Object,
-      default: null
-    }
-  },
-  methods: {
-    reload:function(){
-      var now = new Date().getTime();
-      $('.img1').attr('src','https://smash-bros-stage-select-app.web.app/_nuxt/img/genshi_line_animated.9ce9c6d.svg'+'?'+''+now);
-
-      // ↓localはこっち
-      // $('.img1').attr('src','./_nuxt/static/img/genshi_line_animated.svg'+'?'+''+now);
-
-      $(".img2").removeClass('animated');
-
-      setTimeout(function() {
-        $(".img2").addClass('animated');
-      }, 1000);
+      default: null,
     },
   },
 
-  data () {
+  data() {
     return {
       pageNotFound: '404 Not Found',
       otherError: 'An error occurred',
     }
   },
-  head () {
+  methods: {
+    reload() {
+      const now = new Date().getTime()
+      $('.img1').attr(
+        'src',
+        'https://smash-bros-stage-select-app.web.app/_nuxt/img/genshi_line_animated.9ce9c6d.svg' +
+          '?' +
+          '' +
+          now
+      )
+
+      // ↓localはこっち
+      // $('.img1').attr('src','./_nuxt/static/img/genshi_line_animated.svg'+'?'+''+now);
+
+      $('.img2').removeClass('animated')
+
+      setTimeout(function () {
+        $('.img2').addClass('animated')
+      }, 1000)
+    },
+  },
+  head() {
     const title =
       this.error.statusCode === 404 ? this.pageNotFound : this.otherError
     return {
-      title
+      title,
     }
-  }
+  },
 }
 </script>
 <style scoped>
-  #main{
-    top: 40%;
-    left: 50%;
-    -webkit-transform : translate(-50%,-50%);
-    transform : translate(-50%,-50%);
-  }
+#main {
+  top: 40%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
 
-  #icon {
-    background-color: white;
-  }
+#icon {
+  background-color: white;
+}
 
-  .animation {
-    margin: auto;
-    position: relative;
-  }
+.animation {
+  margin: auto;
+  position: relative;
+}
 
-  .animation img {
-    width: 50%;
-  }
+.animation img {
+  width: 50%;
+}
 
-  .animation .img1 {
-    display: block;
-    margin: 0 auto;
-  }
+.animation .img1 {
+  display: block;
+  margin: 0 auto;
+}
 
-  .animation .img2 {
-    position: absolute;
-    top: 0;
-    left: 25%;
-  }
+.animation .img2 {
+  position: absolute;
+  top: 0;
+  left: 25%;
+}
 
 .img2.animated {
   animation: fade-in 6s;
 }
 
 @keyframes fade-in {
-    0% {
-      opacity: 0;
-    }
+  0% {
+    opacity: 0;
+  }
 
-    12.5% {
-      opacity: 0;
-    }
+  12.5% {
+    opacity: 0;
+  }
 
-    25.5% {
-      opacity: 0;
-    }
-    37.5% {
-      opacity: 0;
-    }
-    50% {
-      opacity: 1;
-    }
+  25.5% {
+    opacity: 0;
+  }
+  37.5% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
 }
-  @keyframes animation {
-    0% {
-      opacity: 0;
-    }
-
-    12.5% {
-      opacity: 0;
-    }
-
-    25.5% {
-      opacity: 0;
-    }
-    37.5% {
-      opacity: 0;
-    }
-    50% {
-      opacity: 1;
-    }
+@keyframes animation {
+  0% {
+    opacity: 0;
   }
 
-  .animation img {
-    animation-name: animation;
-    animation-duration: 7s;
-    animation-iteration-count: 1;
+  12.5% {
+    opacity: 0;
   }
 
-  .animation .img1 {
-    animation-delay:5s;
+  25.5% {
+    opacity: 0;
   }
+  37.5% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+}
 
-  @media screen and (min-width:0px) and ( max-width:600px){
-    #icon{
-      width:300px;
-    }
-  }
+.animation img {
+  animation-name: animation;
+  animation-duration: 7s;
+  animation-iteration-count: 1;
+}
 
-  @media screen and (min-width:600px) and ( max-width:767px){
-    #icon{
-      width:330px;
-    }
-  }
+.animation .img1 {
+  animation-delay: 5s;
+}
 
-  @media screen and (min-width:767px) and ( max-width:960px){
-    #icon{
-      width:430px;
-    }
+@media screen and (min-width: 0px) and (max-width: 600px) {
+  #icon {
+    width: 300px;
   }
+}
 
-  @media screen and (min-width:960px) and ( max-width:1367px){
-    #icon{
-      width:500px;
-    }
+@media screen and (min-width: 600px) and (max-width: 767px) {
+  #icon {
+    width: 330px;
   }
+}
 
-  @media screen and (min-width:1367px) and ( max-width:2504px){
-    #icon{
-      width:500px;
-    }
-    #main{
-      width: 900px;
-    }
+@media screen and (min-width: 767px) and (max-width: 960px) {
+  #icon {
+    width: 430px;
   }
+}
 
-  @media screen and (min-width:2504px) and ( max-width:3000px){
-    #icon{
-      width:544px;
-    }
-    #main{
-      width: 1000px;
-    }
+@media screen and (min-width: 960px) and (max-width: 1367px) {
+  #icon {
+    width: 500px;
   }
+}
+
+@media screen and (min-width: 1367px) and (max-width: 2504px) {
+  #icon {
+    width: 500px;
+  }
+  #main {
+    width: 900px;
+  }
+}
+
+@media screen and (min-width: 2504px) and (max-width: 3000px) {
+  #icon {
+    width: 544px;
+  }
+  #main {
+    width: 1000px;
+  }
+}
 </style>
